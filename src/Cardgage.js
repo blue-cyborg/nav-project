a7000c0a506bf9ee1aeb431b6da56352271bcc18
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-04-19 21:24:58 -0400

    added the annual report section

---
+++ src/Cardgage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import img2 from './images/Senor.jpg';
+
+export default function Cardgage() {
+    return(
+        <div>
+        <img src={img2} alt="A cool guy telling you about numbers" />
+        </div>
+    );
+}
