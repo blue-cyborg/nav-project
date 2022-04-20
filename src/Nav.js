0f36faddeeff284990c8e34173c5f971b99d3da6
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-04-19 20:48:28 -0400

    Update Nav.js
    made the nav links work and added styling to them

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom'
 
 function Nav() {
-  return (
+    const navStyle = {
+        color: 'blue'
+    };
+
+    return (
     <nav>
         <h3>Logo</h3>
         <ul className='nav-Links'>
-            <li>About</li>
-            <li>Annual</li>
-            <li>Events</li>
-            <li>Signup</li>
-            <li>Login</li>
+            <Link style={navStyle} to="/"><li>Home</li></Link>
+            <Link style={navStyle} to="/About"><li>About</li></Link>
+            <Link style={navStyle} to="/Annual"><li>Annual</li></Link>
+            <Link style={navStyle} to="/Events"><li>Events</li></Link>
+            <Link style={navStyle} to="/Signup"><li>Signup</li></Link>
+            <Link style={navStyle} to="/Signin"><li>Login</li></Link>
         </ul>
     </nav>
   );
