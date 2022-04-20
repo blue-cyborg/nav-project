dba5c95c70fd9fb90ac84590f48ebc8302a01de7
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-04-19 22:02:09 -0400

    Added Events Files

--- src/Events.js
+++ src/Events.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Nachos from "./Nachos";
+import Footer from "./Footer";
 
-function Events() {
-  return (
-    <div>
-        <h1>Events</h1>
+export default function Events() {
+    return (
+    <div
+    >
+      <h1>Welcome to Events</h1>
+      <section>
+      <h2>Nacho Party</h2>
+      <p>The Nacho Party event is 3:30 pm on Friday. It will be at a place.
+          Please stop in. It will be fun. There will be nachos. Pay $5 at the door.</p>
+          <Nachos />
+          </section>
+          <Footer />
     </div>
   );
 }
-
-export default Events;
---
+++ src/Nachos.js
@@ -0,0 +1,10 @@
+import React from "react";
+import img3 from './images/Nachos.jpeg';
+
+export default function Nachos() {
+    return(
+        <div>
+        <img src={img3} alt="People Eating" />
+        </div>
+    );
+}
