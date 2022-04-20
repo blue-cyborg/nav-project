4863bf744e76beb1ffcdc739964e53aeff230e53
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-04-19 22:06:35 -0400

    Added Signup page

---
+++ src/Kermit.js
@@ -0,0 +1,10 @@
+import React from "react";
+import img4 from './images/kermit.gif';
+
+export default function Kermit() {
+    return(
+        <div>
+        <img src={img4} alt="get emails from Kermit" />
+        </div>
+    );
+}
--- src/Signup.js
+++ src/Signup.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import './App.css';
+import Kermit from './Kermit';
+import Footer from './Footer';
+import "./SignUp.css";
 
-function Signup() {
+  
+export default function SignUp() {
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("Awesome we will send you cool emails");
+  }
   return (
-    <div>
-        <h1>Sign up</h1>
+    <div className='SignUp'>
+    <div className="container"
+    >
+      <h1>Sign Up</h1>
+      <section>
+      <div className="row">
+        <div className='col'>
+      <Kermit />
+      </div>
+      <div className='col'>
+      <p>sign up for fun things and emails here!</p>
+      <p>We want to send you emails about our sales and events</p>
+      <p>Yay email!</p>
+      <form className="suForm mt-4" onSubmit={handleSubmit}>
+      <input type="text" className="suField" placeholder="Enter your email" />
+      <input type="submit" className="suSubmit" value="Enter" />
+    </form>
+      </div>
+      </div>
+      </section>
+      </div>
+      <Footer />
     </div>
   );
-}
-
-export default Signup;
+};
