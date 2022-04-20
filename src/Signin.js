7ad84eb1fb50449cebdc861a4809c47a72a99d8a
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-04-19 22:22:49 -0400

    added the Signin page

--- src/Signin.js
+++ src/Signin.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import './App.css';
+import "./Signin.css"
+import Superfriends from './Superfriends';
+import Footer from './Footer';
 
-function Signin() {
+export default function Signin() {
+    function handleSubmit(event) {
+    event.preventDefault();
+    alert("Hello! You are now logged in");
+  }
+
   return (
-    <div>
-        <h1>Sign in</h1>
+    <div className='Signin'>
+    <div className="container"
+    >
+           <h1>Welcome Back</h1>
+      <h2 className='account mb-5'>please login to your Super Friends! account</h2>
+      <section>
+      <div className="row">
+        <div className='col'>
+      <Superfriends />
+      </div>
+      <div className='col'>
+
+    <form className="Form" onSubmit={handleSubmit}>
+      <input type="text" className="Username mb-3" placeholder="Username" />
+      <br />
+      <input type="password" className="Password mb-3" placeholder="Password" />
+      <br />
+      <input type="submit" className="Button w-100" value="Login" />
+    </form>
+      </div>
+      </div>
+      </section>
+      </div>
+      <Footer />
     </div>
   );
-}
-
-export default Signin;
+};
