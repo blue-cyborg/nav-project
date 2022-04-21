fe800f1d29a87b4ea7814350273ac792bda41931
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-04-20 21:44:57 -0400

    Update Footer.js
    href'ed my github

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
     return (
         <div className="Footer mt-3">
-            <p>🤖Open source code by Candice Drave🤖</p>
+            <p>🤖<a href="https://github.com/blue-cyborg/nav-project">Open source code</a> by Candice Drave🤖</p>
             </div>
     )
 }
